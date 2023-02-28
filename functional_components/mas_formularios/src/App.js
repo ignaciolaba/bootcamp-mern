376387d80cf6b8a0a364c45db74da03cb2670170
Ignacio Labarca <ignaciolabarca@iMac-de-Ignacio.local>
2023-02-28 10:31:38 -0300

    mas formularios

---
+++ functional_components/mas_formularios/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { Mas_formularios } from './components/Mas_formularios';
+
+function App() {
+  return (
+    <Mas_formularios />
+  );
+}
+
+export default App;
